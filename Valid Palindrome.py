b60f733a645c4fc9fecc34961fc1122b937ab5a3
IDOKO EMMANUEL <pidox31102002@gmail.com>
2023-01-04 15:07:56 +0100

    update

--- Valid Palindrome.py
+++ Valid Palindrome.py
@@ -1,10 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # remove non alpha numeric characters and join strings
+        # join empty spaces together - filter in only alpha numeric characters
         string = ''.join(filter(str.isalnum, s))
         reversedString = ""
+
+        # reverse string in O(n) time
         for i in string:
             reversedString = i + reversedString
         print(reversedString.lower());
         print(string.lower())
+        # Check reversed string against originak string
         return string.lower() == reversedString.lower()
         
