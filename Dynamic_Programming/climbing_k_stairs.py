864d7e8ab4238bb9f4a97a9531d1a1e9b4dda645
IDOKO EMMANUEL <pidox31102002@gmail.com>
2024-11-30 15:48:07 +0000

    memoization

--- Dynamic_Programming/climbing_k_stairs.py
+++ Dynamic_Programming/climbing_k_stairs.py
@@ -1,12 +1,27 @@
+import time
+start_time = time.time()
+
 n = int(input())
 k = int(input())
 
+dp = [-1 for i in range(n+1)]
+
 def number_of_ways(n):
+    
     if n == 0:
         return 1
+    if dp[n] != -1:
+        return dp[n]
     sum_of_ways = 0
+
     for i in range(1, k + 1):
         if n - 1 >= 0:
             sum_of_ways += number_of_ways(n - i)
-    return sum_of_ways
+    dp[n] = sum_of_ways
+    return dp[n]
 print(number_of_ways(n))
+
+
+end_time = time.time()
+execution_time = end_time - start_time
+print("Execution time:",execution_time)
